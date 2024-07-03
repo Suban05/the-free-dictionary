0a03a5a5470aea173eef83fc2c0ee5c06047f8d3
Suban05 <anatolyb94@gmail.com>
2024-07-03 22:29:53 +0500

    refactor `dictionary`

--- lib/the_free_dictionary/dictionary.rb
+++ lib/the_free_dictionary/dictionary.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "net/http"
+require 'net/http'
 
 module TheFreeDictionary
   class Dictionary
@@ -8,44 +8,46 @@ module TheFreeDictionary
 
     def find(statement)
       uri = build_uri(statement)
-      begin
-        response = Net::HTTP.get_response(uri)
-        audio_data = find_audio_data(response)
-        transcription_data = find_transcription_data(response)
-      rescue Socket::ResolutionError
-        audio_data = nil
-        transcription_data = nil
-      end
-      if audio_data.nil?
-        sound = ""
-      else
-        sound = "https://img2.tfd.com/pron/mp3/#{audio_data[0]}.mp3"
-      end
+      response = fetch(uri)
+      sound = build_sound_url(response)
+      transcription = fetch_transcription(response)
 
-      if transcription_data.nil?
-        transcription = ""
-      else
-        transcription = decode_html_entities(transcription_data[1].force_encoding("UTF-8"))
-      end
       { sound:, transcription: }
     end
 
     private
 
+    def fetch(uri)
+      Net::HTTP.get_response(uri)
+    rescue Socket::ResolutionError
+      nil
+    end
+
     def build_uri(statement)
-      URI(URI::Parser.new.escape("https://#{@language}.thefreedictionary.com/#{statement}"))
+      URI(URI::DEFAULT_PARSER.escape("https://#{@language}.thefreedictionary.com/#{statement}"))
     end
 
-    def find_audio_data(response)
-      response.body.match(/#{@language}\/#{@region}[^"]+/)
+    def build_sound_url(response)
+      sound_data = response.body.match(%r{#{@language}/#{@region}[^"]+})
+      if sound_data.nil?
+        ''
+      else
+        "https://img2.tfd.com/pron/mp3/#{sound_data[0]}.mp3"
+      end
     end
 
-    def find_transcription_data(response)
-      response.body.match(/<span[^>]*onclick="pron_key\(1\)"[^>]*class="pron"[^>]*>\s*\(?([^)]+)\)?\s*<\/span>/)
+    def fetch_transcription(response)
+      regex = %r{<span[^>]*onclick="pron_key\(1\)"[^>]*class="pron"[^>]*>\s*\(?([^)]+)\)?\s*</span>}
+      transcription_data = response.body.match(regex)
+      if transcription_data.nil?
+        ''
+      else
+        decode_html_entities(transcription_data[1].force_encoding('UTF-8'))
+      end
     end
 
     def decode_html_entities(string)
-      string.gsub(/&#(\d+);/) { |match| $1.to_i.chr(Encoding::UTF_8) }
+      string.gsub(/&#(\d+);/) { |_match| ::Regexp.last_match(1).to_i.chr(Encoding::UTF_8) }
     end
   end
 end
