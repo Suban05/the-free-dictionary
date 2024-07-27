45ca4556dd3f52d89f143044936c09d87827de9e
Suban05 <anatolyb94@gmail.com>
2024-07-28 01:25:13 +0500

    #2 refactor

--- lib/the_free_dictionary/resources/word_of_day.rb
+++ lib/the_free_dictionary/resources/word_of_day.rb
@@ -8,13 +8,39 @@ module TheFreeDictionary
     class WordOfDay < Resource
       def call
         response = fetch(build_uri(base_url))
-        html = response.body.split("<div id=\"Content_CA_WOD_0_BC\"")[1]
-        doc = Nokogiri::HTML(html, nil, 'UTF-8')
-        word = doc.at_xpath('//h3/a').text.strip
-        definition = doc.at_xpath('//tr[1]/td[2]').text.strip
-        synonyms = doc.at_xpath('//tr[2]/td[2]').text.strip
-        usage = doc.at_xpath('//tr[3]/td[2]').text.strip.sub(/\r\n Discuss\. Play$/, '')
+        doc = nokogiri(response.body)
+        word = find_word(doc)
+        definition = find_definition(doc)
+        synonyms = find_synonyms(doc)
+        usage = find_usage(doc)
         @dictionary.find(word).merge({ word:, definition:, synonyms:, usage: })
+      end
+
+      private
+
+      def nokogiri(string)
+        html = string.split('<div id="Content_CA_WOD_0_BC"')[1]
+        Nokogiri::HTML(html, nil, 'UTF-8')
+      end
+
+      def find_word(nokogiri)
+        find_content(nokogiri, '//h3/a')
+      end
+
+      def find_definition(nokogiri)
+        find_content(nokogiri, '//tr[1]/td[2]')
+      end
+
+      def find_synonyms(nokogiri)
+        find_content(nokogiri, '//tr[2]/td[2]')
+      end
+
+      def find_usage(nokogiri)
+        find_content(nokogiri, '//tr[3]/td[2]').sub(/\r\n Discuss\. Play$/, '')
+      end
+
+      def find_content(nokogiri, expression)
+        nokogiri.at_xpath(expression).text.strip
       end
     end
   end
